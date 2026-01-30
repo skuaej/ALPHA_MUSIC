1920f43dcbc5a69a349dc89c9ee67430c8a1b97f
skuaej <sumankumar821310@gmail.com>
2026-01-30 14:30:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ OWNER_ID = int(getenv("OWNER_ID", 7403621976))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TEAMPURVI/ALPHA_MUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/skuaej/ALPHA_MUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
